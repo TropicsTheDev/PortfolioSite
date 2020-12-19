307500e5301eaa94a7c7823e32c6b38f16ca4b09
Douglas Townsend <dougymt@gmail.com>
2020-12-19 18:45:40 -0500

    removed image optimization component for deployment again

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,12 +1,12 @@
 import styles from "../styles/Header.module.css";
 import Link from "next/link";
-import Image from "next/image";
+
 
 function Header(props) {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Image src="/images/Minilogo.png" width="100px" height="100px" />
+        <img src="/images/Minilogo.png" width="100px" height="100px" />
       </div>
       <nav className={styles.nav}>
         <ul className={styles.nav_links}>
