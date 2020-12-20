ac7b82753da9bca1fa0d99b820703dc9a6d48956
Douglas Townsend <dougymt@gmail.com>
2020-12-19 19:02:20 -0500

    forgot to require path

--- server/app/index.js
+++ server/app/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mailer = require("./mail");
-const { throwError } = require("./utils/errorHandling");
+const path = require("path");
 
 const app = express();
 
